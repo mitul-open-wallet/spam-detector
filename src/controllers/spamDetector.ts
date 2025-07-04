95d9255085551fc7d1c1d6ed4c60588f666a0f79
mitul-open-wallet <contact@openwallet.xyz>
2025-07-05 02:51:30 +0530

    Add log to check whether transaction is a spam

--- src/controllers/spamDetector.ts
+++ src/controllers/spamDetector.ts
@@ -9,9 +9,11 @@ export class SpamDetector {
         this.transactionsFetcher = TransactionsFetcher.getInstance()
     }
 
-    async isSpam(txHash: string, userAdrress: string): Promise<boolean> {
-        const transaction = await this.transactionsFetcher.findTransaction(txHash, userAdrress)
-        return this.transactionsFetcher.isSuspicious(transaction)
+    async isSpam(txHash: string, userAddress: string): Promise<boolean> {
+        const transaction = await this.transactionsFetcher.findTransaction(txHash, userAddress)
+        const isSpam = this.transactionsFetcher.isSuspicious(transaction)
+        console.log(`user address: ${userAddress} with txHash ${txHash} is suspicious ${isSpam}`)
+        return isSpam
     }
 
     async findInfections(userAdrress: string, targetAddress: string | undefined = undefined): Promise<NativeOrContract[]> {
